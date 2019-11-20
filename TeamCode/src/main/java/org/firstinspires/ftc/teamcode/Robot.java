558ebf93d9457272a984b99aaf03eb4482df2022
Nathan Laptop <njmdev03@gmail.com>
2019-11-19 19:10:58 -0600

    Initial commit of command-based programming system examples.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,4 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Robot {
+
+    AutonomousController autonomousController;
+    TeleopController teleopController;
+
+    String programName;
+
+    public Robot(String callerName) {
+        programName = callerName;
+    }
+
+    public void autoInit() {
+
+    }
+
+    public void autoExecute() {
+
+    }
+
+    public void autoEnd() {
+
+    }
+
+    public  void teleopInit() {
+
+    }
+
+    public void teleopExecute() {
+
+    }
+
+    public void teleopEnd() {
+
+    }
 }
